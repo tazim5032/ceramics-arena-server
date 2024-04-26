dc71ebd436dc9e6b044711d55308b0ea4faf000b
tazim5032 <51076835+tazim5032@users.noreply.github.com>
2024-04-27 02:02:44 +0600

    a collection created

--- index.js
+++ index.js
@@ -15,32 +15,44 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const craftCollection = client.db('craftDB').collection('craft');
+
+        //add form theke db te pathabo
+        //client e fetch operation korte hbe..so go to client and send data into body
+        app.post('/addcraft', async (req, res) => {
+            console.log(req.body);
+            const result = await craftCollection.insertOne(req.body);
+            console.log(result);
+            res.send(result); //result jabe client er .then er votore je data ace oikhane
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req,res) =>{
+app.get('/', (req, res) => {
     res.send('Server is running!')
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`server is runnig at port ${port}`)
 })
